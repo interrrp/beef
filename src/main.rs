b3248ce8cd76e0032f075c5fd7c2f880a8d17443
interrrp <watchyourcache@gmail.com>
2024-12-23 10:27:08 +0800

    feat: CLI

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
-fn main() {
-    println!("Hello, world!");
+#![deny(clippy::all)]
+#![warn(clippy::pedantic)]
+
+use std::path::PathBuf;
+
+use anyhow::Result;
+use clap::Parser;
+
+/// A tiny Brainfuck interpreter.
+#[derive(Parser, Debug)]
+struct Args {
+    /// Path of the Brainfuck program to execute.
+    ///
+    /// The filename typically ends in `.bf` or `.b`.
+    program_path: PathBuf,
+}
+
+fn main() -> Result<()> {
+    let args = Args::parse();
+
+    dbg!(args);
+
+    Ok(())
 }
